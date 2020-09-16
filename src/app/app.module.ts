22c95c6515ff52b0226594f46af4bc7fadccafe2
Adam <adamoctav@gmail.com>
2020-09-17 02:50:58 +0300

    Initial Demo

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DynamicFormComponent } from './components/dynamic-form/dynamic-form.component';
+import { FormInputComponent } from './components/form-input/form-input.component';
+import { FormSelectComponent } from './components/form-select/form-select.component';
+import { FormButtonComponent } from './components/form-button/form-button.component';
+import { DynamicFieldDirective } from './components/dynamic-field.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DynamicFormComponent,
+    FormInputComponent,
+    FormSelectComponent,
+    FormButtonComponent,
+    DynamicFieldDirective
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule, 
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/form-input/form-input.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-input',
+  templateUrl: './form-input.component.html',
+  styleUrls: ['./form-input.component.css']
+})
+export class FormInputComponent implements OnInit {
+
+  constructor() { }
+
+  config;
+  group: FormGroup;
+
+  ngOnInit(): void {
+  }
+
+}
