fdf8c19b40f84cf6cbadae05b832e75247fdedc1
Adam <adamoctav@gmail.com>
2021-02-10 00:40:08 +0200

    add validation, multiform, submit support demo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,46 +11,51 @@ import { FormSelectComponent } from './components/form-select/form-select.compon
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  config: UdfForm = {
-    elements:[
-      {
-        type: 'input',
-        name: 'name',
-        value:'',
-        properties:{
-          attr:{
-            placeholder: 'Enter your name',
-            label:'Full name'
+  name1: string = 'TestForm1';
+  name2: string = 'TestForm2';
+  get config(): UdfForm {
+    return {
+      elements: [
+        {
+          type: 'input',
+          name: 'name',
+          value: '',
+          properties: {
+            attr: {
+              placeholder: 'Enter your name',
+              label: 'Full name'
+            }
           }
-        } 
-      },
-      {
-        type:'select',
-        name: 'food',
-        value:'',
-        properties:{
-          attr:{
-            label: 'Favourite food',
-            options: ['Pizza', 'Hot Dogs', 'Knakworstje', 'Coffee'],
-            placeholder: 'Select an option',
+        },
+        {
+          type: 'select',
+          name: 'food',
+          value: '',
+          properties: {
+            attr: {
+              label: 'Favourite food',
+              options: ['Pizza', 'Hot Dogs', 'Knakworstje', 'Coffee'],
+              placeholder: 'Select an option',
+            }
           }
+        },
+        {
+          type: 'button',
+          name: 'submit',
+          value: 'Submit'
         }
-      },
-      {
-        type: 'button',
-        name: 'submit',
-        value: 'Submit'
+      ],
+      components: { // need a better interface that enforces the list
+        input: FormInputComponent,
+        select: FormSelectComponent,
+        button: FormButtonComponent,
       }
-    ]
-  }
-
-  components: UdfComponentList = { // need a better interface that enforces the list
-    input: FormInputComponent,
-    select: FormSelectComponent,
-    button: FormButtonComponent,
+    }
   }
 
   constructor(private udfService: NgUdfService) {
-    this.udfService.setComponents(this.components)
+ 
+    this.udfService.createForm(this.name1, this.config)
+    this.udfService.createForm( this.name2, this.config)
   }
 }
